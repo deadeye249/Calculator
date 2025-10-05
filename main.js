e045812d395f99747401122c50375656514c1590
Deadeye249 <65706696+deadeye249@users.noreply.github.com>
2025-10-05 15:21:29 +0500

    Add Clear and Delete Button Logic

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let numbers_list = "1234567890.".split("");
-let operator_list = "+-x/=".split("");
-let numbers_section = document.querySelector(".numbers-section");
-let operators_section = document.querySelector(".operators-section");
-let screen_section = document.querySelector(".calculator-screen");
-let bottom_section = document.querySelector(".bottom");
+const numbers_list = "1234567890.".split("");
+const operator_list = "+-x/=".split("");
+const numbers_section = document.querySelector(".numbers-section");
+const operators_section = document.querySelector(".operators-section");
+const screen_section = document.querySelector(".calculator-screen");
+const bottom_section = document.querySelector(".bottom");
 const stack = [];
 
 function add(first, second)
@@ -16,7 +16,7 @@ function subtract(first, second)
 }
 function multiply(first,second)
 {
-    parseFloat(first)*parseFloat(second);
+    return parseFloat(first)*parseFloat(second);
 }
 function divide(first,second)
 {
@@ -24,7 +24,7 @@ function divide(first,second)
     {
         return "Error";
     }
-    parseFloat(first)/parseFloat(second);
+    return parseFloat(first)/parseFloat(second);
 }
 
 function operate(second, operator, first)
@@ -48,15 +48,32 @@ function operate(second, operator, first)
 
 function updateDisplay()
 {
-    screen_section.textContent = stack.join(" ");
+    screen_section.textContent = stack.join("");
 }
 
 function updateStack(character)
 {
     if(character == "=")
     {
-        let result = operate(stack.pop(), stack.pop(), stack.pop());
+        const result = operate(stack.pop(), stack.pop(), stack.pop());
         stack.push(result);
+        return;
+    }
+    else if(character == "C")
+    {
+        stack.length = 0;
+        return;
+    }
+    else if(character == "Del")
+    {
+        if(stack[stack.length-1].length>1)
+        {
+            stack[stack.length-1] = stack[stack.length-1].slice(0,-1);
+        }
+        else
+        {
+            stack.pop();
+        }
         return;
     }
     stack.push(character);
@@ -64,7 +81,7 @@ function updateStack(character)
 
 function addButton(parent_node, character)
 {
-    let new_node = document.createElement("div");
+    const new_node = document.createElement("div");
     new_node.textContent = character;
     new_node.className = "button";
     if(parent_node == operators_section)
@@ -85,7 +102,7 @@ function addButton(parent_node, character)
         }
     });
     new_node.addEventListener("click",(event)=>{
-        let clicked_character = event.target.textContent;
+        const clicked_character = event.target.textContent;
         updateStack(clicked_character);
         updateDisplay();
         bottom_section.textContent+=" "+ clicked_character;
@@ -100,3 +117,6 @@ numbers_list.forEach((value)=>{
 operator_list.forEach((value)=>{
     addButton(operators_section,value);
 })
+
+addButton(numbers_section,"C");
+addButton(operators_section,"Del");
